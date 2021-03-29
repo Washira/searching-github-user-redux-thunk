6fcbd29f803c4238ce92fb088d89a4969c3d247c
Washira <us_pou@hotmail.com>
2021-03-30 06:04:22 +0700

    add and adjust codes for redux thunk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { connect } from "react-redux";
+import { thunk_action_creator } from "./actions/fetchAction";
+import UserInfo from "./UserInfo";
 
-function App() {
+function App(props) {
+  let getUsername;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const username = getUsername.value;
+    props.dispatch(thunk_action_creator(username));
+    getUsername.value = "";
+  };
+
+  console.log(props.data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={handleSubmit} className="form">
+        <h2 className="title">Enter the github Username</h2>
+        <input
+          type="text"
+          placeholder="Enter the github Username"
+          required
+          ref={(input) => (getUsername = input)}
+        />
+        <button className="button">Submit</button>
+      </form>
+      {props.data.isFetching ? <h3>Loading ...</h3> : null}
+      {props.data.isError ? (
+        <h3 className="error"> No such user exist.</h3>
+      ) : null}
+      {Object.keys(props.data.userData).length > 0 ? (
+        <UserInfo user={props.data.userData} />
+      ) : null}
     </div>
   );
 }
 
-export default App;
+// receive data from initialState
+const mapStateToProps = (state) => {
+  return {
+    data: state,
+  };
+};
+
+export default connect(mapStateToProps)(App);
